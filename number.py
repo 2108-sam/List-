34689f8cba4fe2abf7d0cbb7806c00b10ea4a755
sam-2108 <okochibwire296@gmailcom>
2026-03-07 14:39:12 +0300

    added

---
+++ number.py
@@ -0,0 +1,12 @@
+number=[1,2,3,4,5]
+print(number)
+new=number.append(4)
+print(number)
+print(new)
+number.insert(2,15)
+print(number)
+view=[12,3,4,5,6]
+print(view)
+number.extend(view)
+print(number)
+
---
+++ sum.py
@@ -0,0 +1,6 @@
+lists=[1,2,3,4,5]
+
+total=0
+for items in lists:
+    total+=items
+print(total)
